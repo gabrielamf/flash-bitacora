3bd39d90132970dad1459a309f4dc7b1b76feea3
Jennifecarmen <jennifercarmens@gmail.com>
2018-02-21 05:17:00 -0500

    añadiendo funcionalidad post imagen, video ,diseño

--- public/js/app.js
+++ public/js/app.js
@@ -136,6 +136,88 @@ $('#date-modal').on('click', function() {
   });
   $('#btn-date').on('click', date);
 });
+function clearInput() {
+  localStorage.url = '';
+  $('.file-path').val('');
+  localStorage.type = '';
+}
 
+function playAudioAndVideo(file, type) {
+  url = URL.createObjectURL(file);
+  localStorage.url = url;
+  localStorage.type = type;
+}
+
+function format() {
+  let fileMA = this.files[0];
+  let type;
+
+  if (fileMA.type.match('audio.*')) {
+    type = 'audio';
+    playAudioAndVideo(fileMA, type);
+  } else if (fileMA.type.match('video.*')) {
+    type = 'video';
+    playAudioAndVideo(fileMA, type);
+  } else {
+    alert('Solo se permite formatos de audio y video');
+  }
+};
+
+
+// -------------- IMAGE --------------- //
+function img() {
+  let imagen = $('#img-guardar');
+
+  const imgTemplate = `<div class="boxMessage z-depth-5 col s12">
+  <img src="${imagen[0].src}" class="col s12 imagen"></img>
+  <div class="right-align"><span class="">${timeDate()}</span></div>
+  </div>`;
+  $('#post').prepend(imgTemplate);
+}
+
+function previewImage() {
+  let file = (this.files[0].name).toString();
+  let reader = new FileReader();
+
+  reader.onload = function(e) {
+    $('#modal1 img').attr('src', e.target.result);
+  };
+  reader.readAsDataURL(this.files[0]);
+};
+
+// Modal image
+$('#img-modal').on('click', function() {
+  $('#img-modal').addClass('waves-effect waves-light modal-trigger');
+  $('.modal').addClass('modal-photo');
+  const modalImgTemplate =
+  `<div class="center">
+        <h6 class="title-post">Elige la imagen que deseas publicar</h6>
+    </div>
+    <form action="#">
+        <div class="file-field input-field">
+            <div class="btn pink lighten-4">
+                <i class="material-icons">linked_camera</i>
+                <input id="file-select" type="file" multiple>
+            </div>
+            <div class="file-path-wrapper">
+                <input class="file-path validate" type="text" placeholder="Elige un photo">
+            </div>
+        </div>
+    </form>
+    <figure class="center">
+        <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNzEiIGhlaWdodD0iMTgwIj48cmVjdCB3aWR0aD0iMTcxIiBoZWlnaHQ9IjE4MCIgZmlsbD0iI2VlZSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9Ijg1LjUiIHk9IjkwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjEycHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+MTcxeDE4MDwvdGV4dD48L3N2Zz4="
+            alt="Previsualizacion Imagen" id="img-guardar" class="responsive-img post-img">
+    </figure>`;
+
+  const modalImgFooterTemplate = `<a id="btn-img" class="modal-action modal-close waves-effect btn-flat purple lighten-3 white-text">
+  <i class="material-icons right">send</i>Public</a>`;
+
+  $('.modal-content').html(modalImgTemplate);
+  $('.modal-footer').html(modalImgFooterTemplate);
+
+  // Preview Image
+  $('input[type=file]').change(previewImage);
+  $('#btn-img').on('click', img);
+});
 
 
